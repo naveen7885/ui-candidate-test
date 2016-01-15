68bf803a6a83b9b0303b13393ddd2599d58c30e7
Naveen Kumar <naveenmails7885@gmail.com>
2016-01-15 00:44:55 -0600

    assignment completion coomit for both stub and text cases

--- jsanddom.js
+++ jsanddom.js
@@ -1,65 +1,104 @@
-     // Example unit test function
+// Example unit test function
      function divide( a, b ) {
-        // To see the test pass, uncomment the following line
-        //return a / b;
+        return a / b;
      }
-
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+          return str.split('').reverse().join('');
      }
-
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+          values.sort(function(a, b) {
+               return a - b;
+          });
+          return values[0];
      }
-
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
-         // FILL THIS IN
+          var result = [];
+          result.push(values[0])
+          for (var i = 1; i < values.length; i++) {
+               if (result.indexOf(values[i]) == -1) {
+                    result.push(values[i]);
+               }
+          }
+          return result;
      }
-
      // Write a function that logs the numbers from 1 to 100 to the console.
      // For multiples of three print "Fizz" instead of the number.
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
-         // FILL THIS IN
-     }
+          for (var i = 1; i <= 100; i++) {
+               if (i % 3 === 0 && i % 5 === 0) {
+                    console.log("FizzBuzz");
+               } else if (i % 3 === 0) {
+                    console.log("Fizz");
+               } else if (i % 5 === 0) {
+                    console.log("Buzz");
+               } else {
+                    console.log(i);
+               }
 
+          }
+     }
      // You have a master array of strings, where each element is a fruit name.
      // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
      // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
      function removeFruits(fruits, fruitsToRemove) {
-         // FILL THIS IN
-     }
+          for (var i = 0; i < fruitsToRemove.length; i++) {
+               var index = fruits.indexOf(fruitsToRemove[i]);
+               fruits.splice(index, 1);
+          }
 
+          console.log(fruits.toString());
+     }
      // Write a function to push either a simple value or an array of values onto a specified array.
      // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+          if (toPush instanceof Array) {
+               for (var i = 0; i < toPush.length; i++) {
+                    array.push(toPush[i]);
+               }
+          } else {
+               array.push(toPush)
+          }
+          return array;
      }
-
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
-         // FILL THIS IN
+          if (sourceStr == "")
+               return [];
+          else
+               return sourceStr.split('').join(',')
      }
-
      // Write a function that will take any number of arguments and return their sum
      function sum() {
-         // FILL THIS IN
+          var sum = 0;
+          for (var i = 0; i < arguments.length; i++) {
+               if (arguments[i] instanceof Array) {
+                    sum = sum + arraySum(arguments[i]);
+               } else {
+                    sum += arguments[i];
+               }
+          }
+          return sum;
      }
-
+     function arraySum(inputar) {
+          var res = 0;
+          for (var j = 0; j < inputar.length; j++) {
+               res += inputar[j];
+          }
+          return res;
+     }
      // Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
          // FILL THIS IN
      }
-
      // write an example of a javascript closure
-
      // define a json object that represents a collection of people.
      // each person should have the following properties
      // - first name
@@ -68,7 +107,31 @@
      // - state
      // - zip
      // - a collection of phone numbers (home, work, mobile)
-
+     var persons = {
+          "values": [{
+               "first name": "abc",
+               "last name": "xyz",
+               "city": "vvv",
+               "state": "qqq",
+               "zip": "123456",
+               "phone": {
+                    "home": "1234567890",
+                    "mobile": "1234567890",
+                    "work": "1234567890"
+               }
+          }, {
+               "first name": "abc",
+               "last name": "xyz",
+               "city": "dd",
+               "state": "dd",
+               "zip": "23457",
+               "phone": {
+                    "home": "1234567890",
+                    "mobile": "1234567890",
+                    "work": "1234567890"
+               }
+          }]
+     };
 
      // Create a javascript object (DataTable) with the following:
      // A private columns property (string array)
@@ -82,12 +145,45 @@
      // .addRow('value1A', 'value1B', 'value1C');
      // .addRow('value2A', 'value2B', 'value2C');
 
+     function DataTable() {
+          var columns = [];
+          var rows = [];
+          this.addRows = function(rowstring) {
+               rows.push(rowstring);
+          };
+          this.addColumns = function(colstring) {
+               columns.push(colstring);
+          };
+          this.getData = function() {
+               var data = {
+                    "rows": rows,
+                    "columns": columns
+               };
+               return data;
+          };
+     }
+
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
+     var div1 = document.getElementById('div1');
+     var slectitems = "<select id='sel'><options><option vlaue='c#'>"+
+         "C#</option><option value='java'>Java</option></options></select><br/><br/><button id='clckbtn'>Click</button>";
+     div1.innerHTML = slectitems;
+     var btn = document.getElementById('clckbtn').addEventListener('click',function() {
+          var val = document.getElementById('sel');
+          console.log(val.value);
+     });
 
      // Write 5 different jQuery selectors to retrieve the
      // sample anchor in the markup below.
+
+     1) $('a')[0]
+     2) $('a[class="link"]')
+     3) $(".link")
+     4)$("#foo>#fizz>a")
+     5)$("#fizz>a")
+
 
      // Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -12,3 +12,61 @@ test( "Example Test", 2, function() {
 /********************************
 Please create your tests below...
 ********************************/
+
+test( "sum Test", 2, function() {
+    // Verify the method exists
+    equal( typeof sum, 'function', 'Must contain a sum function' );
+
+    // Make sure the result from the divide function is valid
+    ok(sum( [2,4], 2 ), 8, 'Expected 8 as the result, the result was: ' + sum( 4, 2 ) );
+});
+
+test( "splitListStrUsingComma Test", 2, function() {
+    // Verify the method exists
+    equal( typeof splitListStrUsingComma, 'function', 'Must contain a sum function' );
+
+    // Make sure the result from the divide function is valid
+    ok(splitListStrUsingComma( 'thanuz'), "t,h,a,n,u,z", 'Expected t,h,a,n,u,z as the result, the result was: ' + splitListStrUsingComma( 'thanuz' ) );
+});
+
+test( "findMinValue Test", 2, function() {
+    // Verify the method exists
+    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' );
+
+    // Make sure the result from the divide function is valid
+    ok(findMinValue([2,0,12,-9]), -9, 'Expected -9 as the result, the result was: ' + findMinValue([2,0,12,-9]) );
+});
+
+
+test( "reverseString Test", 2, function() {
+    // Verify the method exists
+    equal( typeof reverseString, 'function', 'Must contain a reverseString function' );
+
+    // Make sure the result from the divide function is valid
+    ok(reverseString( "abc"), "cba", 'Expected cba as the result, the result was: ' + reverseString("abc") );
+});
+
+test( "findDistinctValues Test", 2, function() {
+    // Verify the method exists
+    equal( typeof findDistinctValues, 'function', 'Must contain a findDistinctValues function' );
+
+    // Make sure the result from the divide function is valid
+    ok(findDistinctValues([1,6,7,2,3,2,7,7]),[1,6,7,2,3], 'Expected [1,6,7,2,3] as the result, the result was: ' + findDistinctValues([1,6,7,2,3,2,7,7]) );
+});
+
+test( "removeFruits Test", 2, function() {
+    // Verify the method exists
+    equal( typeof removeFruits, 'function', 'Must contain a removeFruits function' );
+
+    // Make sure the result from the divide function is valid
+    ok(removeFruits(["apple","grape","banana"],["apple"]), ["grape","banana"], 'Expected ["grape","banana"]  as the result, the result was: ' + removeFruits(["apple","grape","banana"],["apple"]) );
+});
+
+
+test( "pushOntoArray Test", 2, function() {
+    // Verify the method exists
+    equal( typeof pushOntoArray, 'function', 'Must contain a pushOntoArray function' );
+
+    // Make sure the result from the divide function is valid
+    ok(pushOntoArray([1,2,3],4,[5,6]), [1,2,3,4,5,6], 'Expected [1,2,3,4,5,6] as the result, the result was: ' + pushOntoArray([1,2,3],4,[5,6]) );
+});
